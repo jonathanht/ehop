30962bdb0ba692d21e2867baf517d95f4fbf52fb
zknguyen <60521923+zknguyen@users.noreply.github.com>
2020-02-01 17:21:55 -0800

    Add files via upload

--- HackUCI2020.py
+++ HackUCI2020.py
@@ -4,31 +4,41 @@ import requests
 
 # coords of UTC
 
-x = 33.6523 # latitude
-y = -117.8341 # longitude
+x = 33.650160  # latitude
+y = -117.838957  # longitude
 
 # Construct the class
 
 class RestaurantLocator:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, filters: list):
         self._x = x
         self._y = y
         self._BaseURL = "https://api.yelp.com/v3/businesses/search"
         self._ID = "1Vp1MSf8DUcnw-OjS41qzQ" # Client ID
         self._API = "ljefBWbwtGtW4FYiVXEFj5qMXQMBaL08TR1mU6uCa421ZpT7ABj060EfqL7cJQTPRpek-P6-GtmiskfuwihuduS88ie0KDa7dIt2nFDLnQCjWni0aDkcdghjW0M1XnYx" # API Key
         self.headers = {'Authorization': 'Bearer %s' % self._API}
-        self.param = {'term': 'fast food', 'latitude': self._x, 'longitude': self._y}
-        self.filters = ['fast food', 'burgers', 'desserts']
-        self.titles = ["Fast Food", "Burgers", "Desserts"]
-        self.radius = 1500
+        self.param = {'term': 'restaurant, fast food, desserts', 'latitude': self._x, 'longitude': self._y}
+        self.filters = {'fast food': 'Fast Food',
+                        'burgers': 'Burgers',
+                        'desserts': 'Desserts',
+                        'hot dog': 'Hot Dog',
+                        'pizza': 'Pizza',
+                        'barbecue': 'Barbeque',
+                        'chicken shop': 'Chicken Shop',
+                        'chicken wings': 'Chicken Wings'}
+        self.userFilters = {}
+        for item in filters:
+            if item in self.filters.keys():
+                self.userFilters[item] = self.filters[item]
+
+        self.radius = 1000  # ~0.62 miles
+
 
 #   Constructs the url using API,
 
     def constructData(self):
-        #retList = []
         req = requests.get(self._BaseURL, params=self.param, headers=self.headers)
-        #print('The status code is {}'.format(req.status_code))
         data = json.loads(req.text)
 
         return data
@@ -37,6 +47,7 @@ class RestaurantLocator:
 
     def filterResults(self, data):
         retList = []
+        filteredList = []
         for item in data['businesses']:
             if item['distance'] < self.radius:
                 innerList = []
@@ -46,23 +57,28 @@ class RestaurantLocator:
                     innerList.append(cdicts['alias'])
                     innerList2.append(cdicts['title'])
                 for filter in innerList:
-                    if filter in self.filters:
+                    if filter in self.userFilters.keys():
                         filtered = True
                 for title in innerList2:
-                    if title in self.titles:
+                    if title in self.userFilters.values():
                         filtered = True
                 if filtered:
-                    pass
+                    filteredList.append((item['name'], item['distance']))
                 else:
                     retList.append((item['name'], item['distance']))
-        for item in retList:
-            print(item)
-        return retList
+
+        return retList, filteredList
 
 
+# Main function
 
 if __name__ == "__main__":
-    tester = RestaurantLocator(x, y)
+    tester = RestaurantLocator(x, y, ['fast food', 'desserts'])
     m = tester.constructData()
-    tester.filterResults(m)
+    p, q = tester.filterResults(m)
+    for item in p:
+        print(item)
+    print("****")
+    for item in q:
+        print(item)
 
